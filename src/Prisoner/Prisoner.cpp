cb88b9e3608eeef5a5c469def3b7a09d5eae21a4
João Gonçalves <jmgg.goncalves@gmail.com>
2020-05-18 16:57:33 +0100

    prisoner

--- src/Prisoner/Prisoner.cpp
+++ src/Prisoner/Prisoner.cpp
@@ -1,1 +1,24 @@
 #include "Prisoner.h"
+
+Prisoner::Prisoner(int nif, string name, int age, int destination, int location) {
+    this->nif = nif;
+    this->name = name;
+    this->age = age;
+    this->destination = destination;
+    this->location = location;
+}
+
+void Prisoner::printPrisoner() {
+    cout << nif << endl;
+    cout << name << endl;
+    cout << age << endl;
+    cout << destination << endl;
+    cout << location << endl;
+    cout << "::::::::::" << endl;
+}
+
+bool Prisoner::canMove() const {
+    return destination != -1;
+}
+
+
--- src/Prisoner/Prisoner.h
+++ src/Prisoner/Prisoner.h
@@ -1,11 +1,15 @@
 #ifndef SRC_PRISONER_H
 #define SRC_PRISONER_H
+#include <iostream>
+#include <locale>
 #include <string>
 using namespace std;
 
 class Prisoner {
 public:
-    Prisoner(int nif, const string &name, int age, int destination, int location);
+    Prisoner(int nif, string name, int age, int destination, int location);
+    void printPrisoner();
+    bool canMove() const;
 
 private:
     int nif;
@@ -14,10 +18,6 @@ private:
     int destination;
     int location;
 };
-
-Prisoner::Prisoner(int nif, const string &name, int age, int destination, int location){
-    
-}
 
 
 #endif //SRC_PRISONER_H
