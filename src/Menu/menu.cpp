2bb47f1abad45d528507a70763b26bf5885d947f
João Gonçalves <jmgg.goncalves@gmail.com>
2020-05-18 13:01:26 +0100

    welele

--- src/Menu/menu.cpp
+++ src/Menu/menu.cpp
@@ -1,5 +1,4 @@
 #include "menu.h"
-#include <iostream>
 using namespace std;
 
 int showMainMenu()
@@ -7,5 +6,35 @@ int showMainMenu()
     cout << "================================" << endl;
     cout << "              Menu              " << endl;
     cout << "================================" << endl;
+    cout << "1. Ver todos os prisioneiros" << endl;
+    cout << "2. Adicionar um prisioneiro" << endl;
+    cout << "3. Transportar prisioneiro" << endl;
+    cout << "0. Sair" << endl;
+    return choseOption(3);
+}
+
+int choseOption(int options)
+{
+    bool error;
+    string option;
+    do {
+        error = false;
+        cin.clear();
+        getline(cin, option);
+        for (int i = 0; i < option.size(); i++) {
+            if (isalpha(option[i])) {
+                error = true;
+            }
+        }
+        if (error) {
+            cout << "Input inválido" << endl;
+        } else {
+            if (stoi(option) > options || stoi(option) < 0) {
+                error = true;
+                cout << "Input inválido" << endl;
+            }
+        }
+    } while (error);
+    return stoi(option);
 }
 
--- src/Menu/menu.h
+++ src/Menu/menu.h
@@ -1,7 +1,11 @@
 #ifndef MEAT_WAGONS_MENU_H
 #define MEAT_WAGONS_MENU_H
+#include <iostream>
+#include <string>
+#include <locale>
 
 int showMainMenu();
+int choseOption(int options);
 
 #endif //MEAT_WAGONS_MENU_H
 
